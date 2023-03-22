c9e0c5e5701833d9183f67a37b209c8d749c384a
Erique <eriquetoledo@outlook.com>
2023-03-22 12:41:46 -0300

    fix controller usuarios

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -1,17 +1,19 @@
 const sql = require('../db')
 
-function Consultar(req, res){
-    const {id} = req.params
-    
-    let query = `select top 5 * from sup_usuarios where codigo = ${id}`
-
-    sql.query(query, (err, result)=>{
-        if(err){
-            console.warn(`Erro ao buscar usuario: ${err}`)
-        }
-        console.log(result)
-        res.status(200).json(result.recordset)
-    })
+async function Consultar(req, res){
+    const {id} = req.query
+    const query = id ? `select * from sup_usuarios where codigo = ${id}` : 'select * from sup_usuarios' 
+    try {
+        let usuarios = await sql.query(query)
+        
+        const usuario = usuarios.recordset.map(user =>
+                user = {nome:user.Nome, cargo: user.Cargo }
+            )
+        res.status(202).send(usuarios.recordset)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error})
+    }
 }
 
 module.exports = {Consultar}
--- routes/atendimentos.js
+++ routes/atendimentos.js
@@ -10,10 +10,9 @@ routes.post('/atendimentos', atendimentosController.Inserir);
 routes.post('/atendimentos/update', atendimentosController.Atualizar);
 
 // Usuarios
-routes.get('/usuarios/:id', usuarioController.Consultar);
+routes.get('/usuarios', usuarioController.Consultar);
 
 // Login
 routes.post('/login', loginController.Login);
-
 
 module.exports = routes;
