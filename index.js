c73460a417ee1805a41859af613e60c333594a7f
Erique <eriquetoledo@outlook.com>
2023-04-27 13:26:00 -0300

    Config Swagger

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/atendimentos');
 const app = express();
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument  = require('./swagger/swagger.json')
 
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(routes);
 
